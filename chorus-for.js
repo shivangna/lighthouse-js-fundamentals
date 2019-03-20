af87a32a6ff6860391edb296adae3a8ad9568676
Shivangna Kaistha <shivangna.kaistha@gmail.com>
2019-03-20 00:04:35 +0000

    Add all looping exercises

---
+++ chorus-for.js
@@ -0,0 +1,13 @@
+var chorus = "Let's dance!";
+for (var repeat = 0; repeat < 10; repeat++) {
+  console.log(chorus);
+}
+console.log("Until the sun comes up!");
+
+
+// Be careful with the syntax – a for loop requires
+// three statements separated by semicolons.
+// The first is for initializing the loop, most commonly to
+// declare our counter variable, the second for defining our
+//  while condition, and the last for updating the state of our
+// loop after every step.
