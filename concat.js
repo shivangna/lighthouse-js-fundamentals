bdeddcb2698274d471607116f7ebe62bdf5a4d14
Shivangna Kaistha <shivangna.kaistha@gmail.com>
2019-03-20 17:42:55 +0000

    concatanating array values into one

---
+++ concat.js
@@ -0,0 +1,11 @@
+function concat (array1,array2) {
+  for (var i = 0; (i <= (array2.length - 1)); i++) {
+    array1.push(array2 [i]);
+  }
+  return array1;
+  }
+
+
+
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
